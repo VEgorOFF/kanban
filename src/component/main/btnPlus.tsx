3eb7d6653993b2750f893c06840e22b41025a9a8
Vitaliy <vit3r@mail.ru>
2025-06-03 22:00:33 +0500

    first coommit

---
+++ src/component/main/btnPlus.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+import imgBtnPlus from "../../img/plus.svg";
+
+export const BtnPlus = () => {
+  const [isInputVisible, setIsInputVisible] = useState(false);
+  const [nameBtn, setNameBtn] = useState("Add card");
+
+  const [inputValue, setInputValue] = useState("");
+  const [submitedValue, setSubmitedValue] = useState("");
+
+  const addInput = () => {
+    setIsInputVisible(!isInputVisible);
+
+    if (isInputVisible === false) {
+      setNameBtn("Submit");
+    } else {
+      setNameBtn("Add card");
+    }
+  };
+
+  const addCard = () => {
+    setSubmitedValue(inputValue);
+    setIsInputVisible(false);
+    setNameBtn("Add card");
+    setInputValue("");
+  };
+
+  const handleChange = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <>
+      {submitedValue && <p>Отправленные данные: {submitedValue}</p>}
+      {isInputVisible && (
+        <input
+          value={inputValue}
+          onChange={handleChange}
+          type="text"
+          placeholder="Введите текст"
+        />
+      )}
+      <button
+        onClick={() => {
+          if (isInputVisible) {
+            addCard();
+          }
+          if (!isInputVisible) {
+            addInput();
+          }
+        }}
+      >
+        {!isInputVisible && <img src={imgBtnPlus} alt="plus" />}
+        {nameBtn}
+      </button>
+    </>
+  );
+};
